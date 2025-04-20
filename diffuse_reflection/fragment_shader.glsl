fefc5ec6638f0ac5f61accba610d022cfc91eee7
石川 Takeshi <Keshizin@users.noreply.github.com>
2025-04-20 17:08:45 -0300

    Update fragment_shader.glsl

--- diffuse_reflection/fragment_shader.glsl
+++ diffuse_reflection/fragment_shader.glsl
@@ -1,1 +1,11 @@
+#version 420 core
 
+layout (location=0) in vec2 texCoord;
+layout (location=1) in vec4 color;
+
+layout (location=0) out vec4 fragmentColor;
+
+void main()
+{
+	fragmentColor = color;
+}
